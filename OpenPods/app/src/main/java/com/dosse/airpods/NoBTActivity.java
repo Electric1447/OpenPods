6a5a471ac8ca34ace6601d00d22cb260efb9da13
Electric <itai.levin4@gmail.com>
2020-06-14 17:29:09 +0300

    Update compatibility version

--- OpenPods/app/src/main/java/com/dosse/airpods/NoBTActivity.java
+++ OpenPods/app/src/main/java/com/dosse/airpods/NoBTActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 public class NoBTActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_bt);
     }
+
 }
--- OpenPods/app/src/main/java/com/dosse/airpods/SettingsActivity.java
+++ OpenPods/app/src/main/java/com/dosse/airpods/SettingsActivity.java
@@ -1,62 +1,89 @@
 package com.dosse.airpods;
 
+import android.app.AlertDialog;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.util.Log;
 import android.widget.Toast;
 
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
+import static com.dosse.airpods.AboutActivity.donateURL;
+import static com.dosse.airpods.AboutActivity.githubURL;
+import static com.dosse.airpods.AboutActivity.websiteURL;
+
+@SuppressWarnings("deprecation")
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
-    protected void onCreate(Bundle icicle) {
+    protected void onCreate (Bundle icicle) {
         super.onCreate(icicle);
         getPreferenceManager().setSharedPreferencesName("openpods");
-        addPreferencesFromResource(R.xml.pref_general);
+        addPreferencesFromResource(R.xml.preference_screen);
         getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 
-        //hide app listener
-        ((Preference)findPreference("hideApp")).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                PackageManager p = getPackageManager();
-                p.setComponentEnabledSetting(new ComponentName(SettingsActivity.this,MainActivity.class), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-                Toast.makeText(getApplicationContext(),getString(R.string.hideClicked), Toast.LENGTH_LONG).show();
-                try{getApplicationContext().openFileOutput("hidden",MODE_PRIVATE).close();}catch(Throwable t){}
+        // Hide app listener
+        findPreference("hideApp").setOnPreferenceClickListener(preference -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.hide_dialog);
+            builder.setMessage(R.string.hide_dialog_desc);
+            builder.setPositiveButton(R.string.hide_dialog_button, (dialog, which) -> {
+                getPackageManager().setComponentEnabledSetting(new ComponentName(SettingsActivity.this, MainActivity.class), PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                Toast.makeText(this, getString(R.string.hideClicked), Toast.LENGTH_LONG).show();
+
+                try {
+                    openFileOutput("hidden", Context.MODE_PRIVATE).close();
+                } catch (Throwable ignored) {
+                }
+
                 enableDisableOptions();
                 finish();
-                return true;
-            }
+            });
+            builder.setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.dismiss());
+            builder.show();
+            return true;
         });
 
-        //about listener. Removing or hiding this is a violation of the GPL license
-        ((Preference)findPreference("about")).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                Intent i=new Intent(SettingsActivity.this,AboutActivity.class);
-                startActivity(i);
-                return true;
-            }
+        // About listener. Removing or hiding this is a violation of the GPL license
+        findPreference("about").setOnPreferenceClickListener(preference -> {
+            startActivity(new Intent(SettingsActivity.this, AboutActivity.class));
+            return true;
+        });
+
+        findPreference("github").setOnPreferenceClickListener(preference -> {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(githubURL)));
+            return true;
+        });
+
+        findPreference("website").setOnPreferenceClickListener(preference -> {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(websiteURL)));
+            return true;
+        });
+
+        findPreference("donate").setOnPreferenceClickListener(preference -> {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(donateURL)));
+            Toast.makeText(this, "❤️", Toast.LENGTH_SHORT).show();
+            return true;
         });
 
         enableDisableOptions();
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if(key.equalsIgnoreCase("batterySaver")){
+    public void onSharedPreferenceChanged (SharedPreferences sharedPreferences, String key) {
+        if (key.equalsIgnoreCase("batterySaver"))
             Starter.restartPodsService(getApplicationContext());
+    }
+
+    private void enableDisableOptions () {
+        try {
+            getApplicationContext().openFileInput("hidden").close();
+            findPreference("hideApp").setEnabled(false);
+        } catch (Throwable ignored) {
         }
     }
 
-    private void enableDisableOptions(){
-        try{
-            getApplicationContext().openFileInput("hidden").close();
-            ((Preference)findPreference("hideApp")).setEnabled(false);
-        }catch (Throwable t){}
-    }
 }
